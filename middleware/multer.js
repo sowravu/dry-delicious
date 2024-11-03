52374d93b8e438999f25b03134c8e749f26187c2
sowravu <sowravuusuresh@gmail.com>
2024-11-03 14:30:50 +0530

    multer completed

---
+++ middleware/multer.js
@@ -0,0 +1,30 @@
+
+const multer = require("multer");
+
+const path=require("path")
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); 
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname)); 
+  },
+});
+const fileFilter = (req, file, cb) => {
+  const fileTypes = /jpeg|jpg|png/;
+  const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = fileTypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb(new Error("Only images are allowed (jpeg, jpg, png)"));
+  }
+};
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 5 }, 
+  fileFilter: fileFilter,
+});
+
+module.exports =upload;
