e1706115240517a758680fe1067f591e546406df
sowravu suresh <sowravuusuresh@gmail.com>
2025-01-08 13:47:21 +0530

    updated mongodb

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const MONGO_URI = process.env.MONGODB_URI;
 mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    
   })
   .then(() => console.log("MongoDB connected successfully"))
   .catch((error) => {
